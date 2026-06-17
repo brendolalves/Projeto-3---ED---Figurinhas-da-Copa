87079a9aa5a71767b46cbb6ec9761056adb040bf
brendolalves <brendol.alves@gmail.com>
2026-06-17 09:39:02 -0300

    criação do estrutura.py

---
+++ estrutura.py
@@ -0,0 +1,10 @@
+import csv
+import random
+
+class figurinhas:
+    def __init__(self, id_fig, nome, pais, posicao, raridade):
+        self.id = int(id_fig)
+        self.nome = nome
+        self.pais = pais
+        self.posicao = posicao
+        self.raridade = raridade
